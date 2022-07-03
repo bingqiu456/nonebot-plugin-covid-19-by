efe842d73c118d4ae3d6b5bd7ca346481b0581fc
bingyue <99388013+bingqiu456@users.noreply.github.com>
2022-07-03 17:22:09 +0800

    Update config_covid_19.py

--- nonebot_plugin_covid_19_by/config_covid_19.py
+++ nonebot_plugin_covid_19_by/config_covid_19.py
@@ -7,21 +7,21 @@ from nonebot.adapters.onebot.v11 import Event,GROUP_OWNER,GROUP_ADMIN,GroupMessa
 global colour
 global size
 global group_image_covid
-colour = get_driver().config.dict().get("covid_19_by_colour",None)
+colour = get_driver().config.dict().get("covid_19_by_colour","#ccffcc")
 group_image_covid = get_driver().config.dict().get("covid_19_by_images",[])
-size = get_driver().config.dict().get("covid_19_by_size",None)
+size = get_driver().config.dict().get("covid_19_by_size",20)
 global group_covid
 group_covid = get_driver().config.dict().get("covid_19_by_group",[])
 if colour ==None:
-    logger.warning("covid_19:未配置文转图颜色 默认None")
+    logger.warning("covid_19:未配置文转图颜色 默认#ccffcc")
 if size==None:
-    logger.warning("covid_19:未配置文转图字体 默认None")
+    logger.warning("covid_19:未配置文转图字体大小 默认20")
 if group_covid ==[]:
     logger.warning("covid_19:未配置开启群号 默认[]")
 if group_image_covid==[]:
     logger.success("covid_19:未配置文转图群组 默认[]")
 
-add_group = on_command("covid_19_by_group_turn_on",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER)
+add_group = on_command("covid_19开启",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER,priority=30)
 @add_group.handle()
 async def _(event:GroupMessageEvent):
     group_covid.append(str(event.group_id))
@@ -29,7 +29,7 @@ async def _(event:GroupMessageEvent):
     await add_group.finish("covid_19:开启本群成功")
     
 
-del_group = on_command("covid_19_by_group_turn_off",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER)
+del_group = on_command("covid_19关闭",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER,priority=30)
 @del_group.handle()
 async def del_group_(event:GroupMessageEvent):
     try:
@@ -41,7 +41,7 @@ async def del_group_(event:GroupMessageEvent):
         logger.success(f"covid_19:关闭{event.group_id}成功")
         await del_group.finish("covid_19:关闭本群成功")
 
-image_group = on_command("covid_19_by_image_turn_on",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER)
+image_group = on_command("疫情文转图开",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER,priority=30)
 @image_group.handle()
 async def _(event:GroupMessageEvent):
     group_image_covid.append(str(event.group_id))
@@ -49,7 +49,7 @@ async def _(event:GroupMessageEvent):
     await image_group.finish("covid_19:开启本群成功")
     
 
-image_group_off = on_command("covid_19_by_image_turn_off",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER)
+image_group_off = on_command("疫情文转图关",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER,priority=30)
 @image_group_off.handle()
 async def del_group_(event:GroupMessageEvent):
     try:
@@ -60,14 +60,3 @@ async def del_group_(event:GroupMessageEvent):
     else:
         logger.success(f"covid_19:关闭{event.group_id}成功")
         await image_group_off.finish("covid_19:关闭本群成功")
-
-
-
-
-
-
-
-
-    
-
-
