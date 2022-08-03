5aa97231437bb9228636ddb9d88069d5ad4e5a53
bingyue <99388013+bingqiu456@users.noreply.github.com>
2022-08-04 00:08:51 +0800

    🍒优化.....

--- nonebot_plugin_covid_19_by/config_covid_19.py
+++ nonebot_plugin_covid_19_by/config_covid_19.py
@@ -1,8 +1,7 @@
 from nonebot.permission import SUPERUSER
 from nonebot import get_driver,on_command
 from loguru import logger
-from nonebot.adapters.onebot.v11 import Event,GROUP_OWNER,GROUP_ADMIN,GroupMessageEvent
-
+from nonebot.adapters.onebot.v11 import GROUP_ADMIN,GROUP_OWNER,GroupMessageEvent
 
 global colour
 global size
@@ -12,28 +11,23 @@ group_image_covid = get_driver().config.dict().get("covid_19_by_images",[])
 size = get_driver().config.dict().get("covid_19_by_size",20)
 global group_covid
 group_covid = get_driver().config.dict().get("covid_19_by_group",[])
-if colour ==None:
-    logger.warning("covid_19:未配置文转图颜色 默认#ccffcc")
-if size==None:
-    logger.warning("covid_19:未配置文转图字体大小 默认20")
 if group_covid ==[]:
     logger.warning("covid_19:未配置开启群号 默认[]")
 if group_image_covid==[]:
     logger.success("covid_19:未配置文转图群组 默认[]")
 
-add_group = on_command("covid_19开启",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER,priority=30)
-@add_group.handle()
+add_group_123 = on_command("covid_19开启",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER,priority=30)
+@add_group_123.handle()
 async def _(event:GroupMessageEvent):
-    group_covid.append(str(event.group_id))
+    group_covid.append(event.group_id)
     logger.success(f"covid_19:开启{event.group_id}成功")
-    await add_group.finish("covid_19:开启本群成功")
+    await add_group_123.finish("covid_19:开启本群成功")
     
-
 del_group = on_command("covid_19关闭",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER,priority=30)
 @del_group.handle()
 async def del_group_(event:GroupMessageEvent):
     try:
-        group_covid.remove(str(event.group_id))
+        group_covid.remove(event.group_id)
     except(ValueError):
         logger.error("covid_19:此群不存在列表中")
         pass
@@ -44,16 +38,15 @@ async def del_group_(event:GroupMessageEvent):
 image_group = on_command("疫情文转图开",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER,priority=30)
 @image_group.handle()
 async def _(event:GroupMessageEvent):
-    group_image_covid.append(str(event.group_id))
+    group_image_covid.append(event.group_id)
     logger.success(f"covid_19:开启{event.group_id}成功")
     await image_group.finish("covid_19:开启本群成功")
     
-
 image_group_off = on_command("疫情文转图关",permission=SUPERUSER|GROUP_ADMIN|GROUP_OWNER,priority=30)
 @image_group_off.handle()
 async def del_group_(event:GroupMessageEvent):
     try:
-        group_image_covid.remove(str(event.group_id))
+        group_image_covid.remove(event.group_id)
     except(ValueError):
         logger.error("covid_19:此群不存在列表中")
         pass
